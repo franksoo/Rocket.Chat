230140f4a94166b847c7daf15f539d7da4ccc8b0
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2024-08-14 18:30:22 -0300

    refactor: RemoveExtensionButton

--- apps/meteor/client/views/admin/users/voip/RemoveExtensionButton.tsx
+++ apps/meteor/client/views/admin/users/voip/RemoveExtensionButton.tsx
@@ -1,50 +1,46 @@
 import { Box, IconButton } from '@rocket.chat/fuselage';
-import { useMutableCallback } from '@rocket.chat/fuselage-hooks';
-import { useSetModal, useToastMessageDispatch, useEndpoint, useTranslation, useUser } from '@rocket.chat/ui-contexts';
-import { useQueryClient } from '@tanstack/react-query';
-import type { FC } from 'react';
+import { useEffectEvent } from '@rocket.chat/fuselage-hooks';
+import { useSetModal, useToastMessageDispatch, useEndpoint, useUser } from '@rocket.chat/ui-contexts';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 import GenericModal from '../../../../components/GenericModal';
 import { GenericTableCell } from '../../../../components/GenericTable';
 
-const RemoveExtensionButton: FC<{ username: string; extension: string }> = ({ username, extension }) => {
+const RemoveExtensionButton = ({ username, extension }: { username: string; extension: string }) => {
 	const removeExtension = useEndpoint('POST', '/v1/voip-freeswitch.extension.assign');
 	const setModal = useSetModal();
 	const dispatchToastMessage = useToastMessageDispatch();
-	const t = useTranslation();
+	const { t } = useTranslation();
 	const queryClient = useQueryClient();
 	const loggedUser = useUser();
 
-	const handleRemoveClick = useMutableCallback(async () => {
-		try {
-			await removeExtension({ username });
-			queryClient.invalidateQueries(['users.list']);
+	const handleRemove = useMutation({
+		mutationFn: ({ username }: { username: string }) => removeExtension({ username }),
+		onSuccess: () => {
+			dispatchToastMessage({ type: 'success', message: t('Extension_removed') });
 
+			queryClient.invalidateQueries(['users.list']);
 			if (loggedUser?.username === username) {
 				queryClient.invalidateQueries(['voice-call-client']);
 			}
-		} catch (error: unknown) {
+
+			setModal(null);
+		},
+		onError: (error) => {
 			dispatchToastMessage({ type: 'error', message: error });
-		}
+			setModal(null);
+		},
 	});
 
-	const handleDelete = useMutableCallback((e) => {
+	const handleDelete = useEffectEvent((e) => {
 		e.stopPropagation();
-		const onDeleteExtension = async (): Promise<void> => {
-			try {
-				await handleRemoveClick();
-				dispatchToastMessage({ type: 'success', message: t('Extension_removed') });
-			} catch (error: unknown) {
-				dispatchToastMessage({ type: 'error', message: error });
-			}
-			setModal();
-		};
 
 		setModal(
 			<GenericModal
 				variant='danger'
-				onConfirm={onDeleteExtension}
+				onConfirm={() => handleRemove.mutate({ username })}
 				onCancel={(): void => setModal()}
 				onClose={(): void => setModal()}
 				confirmText={t('Delete')}
@@ -55,7 +51,8 @@ const RemoveExtensionButton: FC<{ username: string; extension: string }> = ({ us
 	return (
 		<GenericTableCell fontScale='p2' color='hint' withTruncatedText>
 			<Box display='flex' alignItems='center'>
-				<IconButton icon='trash' small title={t('Remove_Association')} onClick={handleDelete} /> {extension}
+				<IconButton disabled={handleRemove.isLoading} icon='trash' small title={t('Remove_Association')} onClick={handleDelete} />{' '}
+				{extension}
 			</Box>
 		</GenericTableCell>
 	);
